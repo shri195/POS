ef4b04a57440e48c8359d54b72b64dcdd14d61a2
Shrikant Jadhav <Shrikant.Jadhav@harbingergroup.com>
2022-08-09 13:49:31 +0530

    Pos initial code

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php $config = array(
+    "database_type"=>"mysql",
+    "database_name"=>"dbtrack",
+    "server"=>"localhost",
+    "username"=>"root",
+    "password"=>"",
+    "charset"=>"utf8",
+    "port"=>3306,
+    "encryption_key"=>"e63ab8bc9d446ea6be0f75261528a8ea7f8aa3be97c2436d3abb53931211701c" ); ?>
